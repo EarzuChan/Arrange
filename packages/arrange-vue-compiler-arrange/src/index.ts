69456889b3aee24bce3b8c4618c2eca64fed3da1
Yachiyo <yachiyo@tsukuyomi.web>
2026-09-19 14:46:55 +0800

    chore: plan M2.4

--- packages/arrange-vue-compiler-arrange/src/index.ts
+++ packages/arrange-vue-compiler-arrange/src/index.ts
@@ -16,18 +16,24 @@ export { parserOptions }
 // 只校验原厂宿主输入，用户组件自行定义 props 与事件
 const validateNativeTemplate: NodeTransform = (node, context) => {
     if (node.type !== NodeTypes.ELEMENT) return
-    const fail = (message: string, loc = node.loc) => {
-        context.onError(Object.assign(new SyntaxError(message), { code: 'ARRANGE_TEMPLATE', loc }))
-    }
+
+    const fail = (message: string, loc = node.loc) => {context.onError(Object.assign(new SyntaxError(message), { code: 'ARRANGE_TEMPLATE', loc }))}
+
     const host = isHostTag(node.tag) ? node.tag : null
+
     for (const prop of node.props) {
-        const name = prop.type === NodeTypes.ATTRIBUTE ? prop.name
-            : prop.name === 'bind' && prop.arg?.type === NodeTypes.SIMPLE_EXPRESSION && prop.arg.isStatic ? prop.arg.content : null
+        const name = prop.type === NodeTypes.ATTRIBUTE ? prop.name : prop.name === 'bind' && prop.arg?.type === NodeTypes.SIMPLE_EXPRESSION && prop.arg.isStatic ? prop.arg.content : null
+
         if (host && name && !['key', 'ref', 'ref_for', 'ref_key'].includes(name) && !acceptsHostInput(host, name)) fail(`Arrange <${host}> 没有输入 ${name}，请检查组件 schema`, prop.loc)
+
         if (prop.type !== NodeTypes.DIRECTIVE) continue
+
         if (!['bind', 'on', 'model', 'if', 'else', 'else-if', 'for', 'slot', 'once', 'memo'].includes(prop.name)) fail(`Arrange 缺少 v-${prop.name} 的宿主转换`, prop.loc)
+
         if (prop.modifiers.length && !(prop.name === 'bind' && prop.modifiers.every(modifier => modifier.content === 'camel'))) fail(`Arrange v-${prop.name} 没有匹配的修饰符转换`, prop.loc)
+
         if (host && prop.name === 'model' && host !== 'Input') fail(`Arrange <${host}> 没有受控输入模型`, prop.loc)
+        
         if (host && prop.name === 'on' && prop.arg?.type === NodeTypes.SIMPLE_EXPRESSION && prop.arg.isStatic && !acceptsHostInput(host, toHandlerKey(camelize(prop.arg.content)))) fail(`Arrange <${host}> 没有事件 ${prop.arg.content}`, prop.loc)
     }
 }
--- packages/runtime/src/components.ts
+++ packages/runtime/src/components.ts
@@ -3,17 +3,25 @@ import { useContentColor } from "./local.ts"
 import type { BoxProps, RowProps, ColumnProps, SpacerProps, TextProps, InputProps, ImageProps, IconProps, HostComponent } from './componentTypes.ts'
 
 export const Box = "Box" as unknown as HostComponent<BoxProps>
+
 export const Row = "Row" as unknown as HostComponent<RowProps>
+
 export const Column = "Column" as unknown as HostComponent<ColumnProps>
+
 export const Spacer = "Spacer" as unknown as HostComponent<SpacerProps>
+
 export const Text = "Text" as unknown as HostComponent<TextProps>
+
 export const Input = "Input" as unknown as HostComponent<InputProps>
+
 export const Image = "Image" as unknown as HostComponent<ImageProps>
+
 export const Icon = defineComponent({
     name: "Icon",
     inheritAttrs: false,
     setup(_props, { attrs, slots }) {
         const contentColor = useContentColor()
+
         return () => h("Icon", {
             ...attrs,
             tint: attrs.tint == null ? contentColor : attrs.tint,
